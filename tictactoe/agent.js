93086706635bfeb47c1bc5883e2b032b528a7273
elichen <eli.chen@gmail.com>
2024-10-04 15:02:17 -0700

    update

--- tictactoe/agent.js
+++ tictactoe/agent.js
@@ -1,5 +1,5 @@
 class DQNAgent {
-  constructor(epsilon = 1.0, epsilonDecay = 0.995, epsilonMin = 0.0, gamma = 0.95, batchSize = 150, maxMemorySize = 100000) {
+  constructor(epsilon = 1.0, epsilonDecay = 0.999, epsilonMin = 0.1, gamma = 0.99, batchSize = 150, maxMemorySize = 100000) {
     this.model = new TicTacToeModel();
     this.epsilon = epsilon;
     this.epsilonDecay = epsilonDecay;
