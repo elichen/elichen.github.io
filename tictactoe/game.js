228d14c1294910ed5d4a5f145cd4ea3aa89c98ba
elichen <eli.chen@gmail.com>
2024-10-15 04:34:45 -0700

    update

--- tictactoe/game.js
+++ tictactoe/game.js
@@ -28,19 +28,9 @@ class TicTacToeGame {
   }
 
   checkGameOver() {
-    // Check rows, columns, and diagonals for a win
-    const winPatterns = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-      [0, 4, 8], [2, 4, 6] // Diagonals
-    ];
-
-    for (const pattern of winPatterns) {
-      const [a, b, c] = pattern;
-      if (this.board[a] !== 0 && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
-        this.gameOver = true;
-        return;
-      }
+    if (this.checkWin(1) || this.checkWin(-1)) {
+      this.gameOver = true;
+      return;
     }
 
     if (!this.board.includes(0)) {
@@ -90,12 +80,14 @@ class TicTacToeGame {
   }
 
   getGameOverMessage() {
-    if (this.board.includes(0)) {
-      const isAgentWin = this.board.filter(cell => cell !== 0).length % 2 !== 0;
-      const winner = isAgentWin ? "Agent (X)" : "Opponent (O)";
-      return `${winner} wins!`;
-    } else {
+    if (this.checkWin(1)) {
+      return "Agent (X) wins!";
+    } else if (this.checkWin(-1)) {
+      return "Opponent (O) wins!";
+    } else if (!this.board.includes(0)) {
       return "It's a draw!";
+    } else {
+      return "Game in progress";
     }
   }
 
