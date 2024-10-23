3f2b805b32b89aebe51444387269e2313e0ae326
elichen <eli.chen@gmail.com>
2024-10-22 19:30:24 -0700

    train offline

--- breakout/human.js
+++ breakout/human.js
@@ -1,16 +1,34 @@
 class HumanPlayer {
     constructor(game) {
         this.game = game;
+        this.keys = {
+            ArrowLeft: false,
+            ArrowRight: false
+        };
         this.setupControls();
     }
 
     setupControls() {
         document.addEventListener('keydown', (e) => {
-            if (e.key === 'ArrowLeft') {
-                this.game.movePaddle('left');
-            } else if (e.key === 'ArrowRight') {
-                this.game.movePaddle('right');
+            if (e.key in this.keys) {
+                this.keys[e.key] = true;
             }
         });
+
+        document.addEventListener('keyup', (e) => {
+            if (e.key in this.keys) {
+                this.keys[e.key] = false;
+            }
+        });
+
+        // Add method to check keys and move paddle
+        this.checkKeys = () => {
+            if (this.keys.ArrowLeft) {
+                this.game.movePaddle('left');
+            }
+            if (this.keys.ArrowRight) {
+                this.game.movePaddle('right');
+            }
+        };
     }
 }
