713ad623b95bb65b5bfaa6ed8076c1e9bfe8c111
elichen <eli.chen@gmail.com>
2025-01-12 11:18:40 -0800

    change total steps to match reference impl

--- streaming-rl/agent.js
+++ streaming-rl/agent.js
@@ -5,7 +5,7 @@ class StreamQ {
         this.epsilonStart = config.epsilonStart || 1.0;
         this.epsilonTarget = config.epsilonTarget || 0.01;
         this.explorationFraction = config.explorationFraction || 0.05;
-        this.totalSteps = config.totalSteps || 100000;
+        this.totalSteps = config.totalSteps || 500000;
         this.timeStep = 0;
         this.epsilon = this.epsilonStart;
 
