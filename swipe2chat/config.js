f10a9ed9a94698ec0dbce55910db279b32fb8dd9
Eli Chen <eli.chen@gmail.com>
2025-02-15 23:50:49 -0800

    initial

---
+++ swipe2chat/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDMNND-7YZPBm3efARoX2QBaLvidx6l-Cg",
+    authDomain: "swipe2chat.firebaseapp.com",
+    projectId: "swipe2chat",
+    storageBucket: "swipe2chat.firebasestorage.app",
+    messagingSenderId: "540585323403",
+    appId: "1:540585323403:web:951097e5a02bec58715d8e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig); 
